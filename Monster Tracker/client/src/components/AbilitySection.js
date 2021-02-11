cbad51ba589041a734c45a735133412f74c3e49a
Austin Ring <aring97@gmail.com>
2021-02-11 11:19:42 -0600

    finished

--- Monster Tracker/client/src/components/AbilitySection.js
+++ Monster Tracker/client/src/components/AbilitySection.js
@@ -1,7 +1,7 @@
 ﻿import React from "react";
 
 const abilitySection = ({ ability, type }) => {
-
+    console.log(ability)
     return (
         type == null ? (<p>{ability}</p>) :
             (ability.type == type ? (<p>{ability.name}: {ability.description}</p>) : (<></>))
--- Monster Tracker/client/src/pages/MonsterDetails.js
+++ Monster Tracker/client/src/pages/MonsterDetails.js
@@ -5,9 +5,11 @@ import { useHistory,useParams } from "react-router-dom";
 import AbilitySection from "../components/AbilitySection";
 import "./monsterDetails.css"
 
+
 export const MonsterDetails = (props) => {
     const { getToken } = useContext(UserContext);
     const [monsterDetails, setMonsterDetails] = useState({});
+    const [abilityTypes, setAbilityTypes] = useState([]);
     const { monsterId } = useParams();
     const monster = monsterDetails.monster;
     const history = useHistory();
@@ -22,9 +24,12 @@ export const MonsterDetails = (props) => {
             .then((res) => res.json())
             .then((monsterDetails) => {
                 setMonsterDetails(monsterDetails);
-            });
+            }).then();
     }, []);
 
+    useEffect(() => {
+        getAbilityTypeArray()
+    }, [monsterDetails]);
     const getMoveString = () => {
 
         let moveString = `Speed: ${monster.walk}ft.`
@@ -53,15 +58,20 @@ export const MonsterDetails = (props) => {
         })
         return senseString;
     }
-    const getAbilities = () => {
-        let stringArray = new Array()
-        monsterDetails.abilities.forEach(ability => {
-            if (ability.type == "Ability") {
-                stringArray.push(ability.name + `: ` + ability.description+"\n");
-            }
-        })
-        return stringArray;
+
+    const getAbilityTypeArray = () => {
+        const typeArray = new Array();
+        monster!=null?(
+        monsterDetails.abilities.map((ability) => {
+            typeArray.push(ability.type)
+        })):(<></>)
+        setAbilityTypes(typeArray);
     }
+
+    
+
+    console.log(monsterDetails)
+    console.log(abilityTypes)
     return (
         monsterDetails.monster != null ? (
             <div className="container">
@@ -90,13 +100,70 @@ export const MonsterDetails = (props) => {
                         <p>{getSense()}</p>
                         <p>Languages: {monster.languages}</p>
                         <p>Challenge: {monster.cr} ({monster.xp}XP)</p>
-                        <hr className="hr-color" />
-                        {monsterDetails.abilities.map((ability) => (
-                            <AbilitySection ability={ability} type={"Ability"} />
-                        ))}
+                        {abilityTypes.includes("Ability") ? (<div>
+                            <hr className="hr-color" />
+                            {monsterDetails.abilities.map((ability) => (
+                                <AbilitySection ability={ability} type={"Ability"} />
+                            ))}
+                        </div>) : (<></>)}
                         {monster.spellList!=null?(monster.spellList.split("@").map((spell) => (
                             <AbilitySection ability={spell} type={null} />
-                        ))):(<></>)}
+                        ))) : (<></>)}
+
+                        {abilityTypes.includes("Action") ? (
+                            <div>
+                                <h3>Actions</h3>
+                                <hr className="hr-color"/>
+                            {
+                                monsterDetails.abilities.map((ability) => (
+                                    <AbilitySection ability={ability} type="Action" />
+                                ))
+                                }
+                        </div>
+                        ) : (<></>)}
+
+                        {abilityTypes.includes("Reaction") ? (
+                            <div>
+                                <h3>Reactions</h3>
+                                <hr className="hr-color" />
+                                {
+                                    monsterDetails.abilities.map((ability) => (
+                                        <AbilitySection ability={ability} type="Reaction" />
+                                    ))
+                                }
+                            </div>
+                        ) : (<></>)}
+
+                        {abilityTypes.includes("Legendary Action") ? (
+                            <div>
+                                <h3>Legendary Actions</h3>
+                                <hr className="hr-color" />
+                                <p>The {monsterDetails.monster.name} can take 3 Legendary Actions, choosing from the options below. Only one legendary action can be used at a time, and only at the end of another creature's turn. The {monsterDetails.monster.name} regains spent legendary Actions at the start of its turn.</p>
+                                {
+                                    monsterDetails.abilities.map((ability) => (
+                                        <AbilitySection ability={ability} type="Legendary Action" />
+                                    ))
+                                }
+                            </div>
+                        ) : (<></>)}
+               
+                        {monsterDetails.abilities.map((ability) => {
+                            if (ability.type == "Lair Actions") {
+                                return (
+                                    <div>
+                                        <h3>Lair Action</h3>
+                                        <hr className="hr-color" />
+                                        <p>On initiative count 20 (losing initiative ties) the {monsterDetails.monster.name} can take a lair action to cause one of the following magical effects: the {monsterDetails.monster.name} can't use the same effect two rounds in a row:</p>
+                                        {
+                                            ability.description.split("-").map((LA) => (
+                                            
+                                            <AbilitySection ability={LA} type={ null}/>
+                                        ))}
+                                    </div>
+                                    )
+                            }
+                        })}
+
                     </div>
                 </div>
                 <p className="btn btn-info" onClick={() => { history.goBack() }}>Go Back</p>
